fdf43d4826077f3328f9cd131a1fe5d774ace9fa
Francisco <marvinfran01@gmail.com>
2025-05-30 10:47:58 -0600

    Video 40-impresi[on-de-variables-HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,21 @@
+//Los Objetos no son permitidos
+    // const newMessage = {
+    //     message: 'Hola Mundo',  
+    //     title: 'Fran'
+
+    // };
+
+    // const getMensaje = () => {
+    //     return 4 + 4;
+    // }
+
 export const FirstApp = () => {
-  return (
+
+    return (
     <>
+    {/*<code>{JSON.stringify(newMessage) }</code> */}
     <h1>Francisco</h1>
-    <p>Spy un subtitulo</p>
+    <p>Soy un subtitulo</p>
     </>
   )
 }
